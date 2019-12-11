789de0d83dcfb7b7989db2e20a84381602f28ba9
hsaruta1 <hsaruta1@gw.nsw.co.jp>
2019-12-11 09:10:04 +0900

    k-means Sample

---
+++ k_means/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace k_means
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numClusters = new System.Windows.Forms.NumericUpDown();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numClusters)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(666, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numClusters
+            // 
+            this.numClusters.Location = new System.Drawing.Point(425, 449);
+            this.numClusters.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numClusters.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numClusters.Name = "numClusters";
+            this.numClusters.Size = new System.Drawing.Size(58, 19);
+            this.numClusters.TabIndex = 1;
+            this.numClusters.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numClusters.ValueChanged += new System.EventHandler(this.numClusters_ValueChanged);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(134, 442);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "元に戻す";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Location = new System.Drawing.Point(13, 4);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(75, 23);
+            this.selectImageButton.TabIndex = 5;
+            this.selectImageButton.Text = "画像選択";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.selectImageButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "クラスター数　k=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "k-means法による減色処理";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(326, 451);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 482);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.numClusters);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "k-means法";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numClusters)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numClusters;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ k_means/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Imaging.Converters;
+using Accord.MachineLearning;
+using Accord.Math;
+using Accord.Statistics.Distributions.DensityKernels;
+using Accord.Math.Distances;
+
+namespace k_means
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap img = null;
+        public string filePath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runKMeans()
+        {            
+            int k = (int)numClusters.Value;
+            
+            Bitmap image = img;
+           
+            ImageToArray imageToArray = new ImageToArray(min: -1, max: +1);
+            ArrayToImage arrayToImage = new ArrayToImage(image.Width, image.Height, min: -1, max: +1);
+            
+            double[][] pixels; imageToArray.Convert(image, out pixels);
+            
+            KMeans kmeans = new KMeans(k, new SquareEuclidean())
+            {
+                Tolerance = 0.05
+            };
+          
+            int[] idx = kmeans.Learn(pixels).Decide(pixels);
+            
+            pixels.Apply((x, i) => kmeans.Clusters.Centroids[idx[i]], result: pixels);
+           
+            Bitmap result; arrayToImage.Convert(pixels, out result);
+
+            pictureBox1.Image = result;
+        }
+
+        private void selectImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofDialog = new OpenFileDialog();
+
+            // デフォルトのフォルダを指定する
+            ofDialog.InitialDirectory = @"C:";
+
+            //ダイアログのタイトルを指定する
+            ofDialog.Title = "画像ファイル選択";
+
+            //ダイアログを表示する
+            if (ofDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofDialog.FileName;
+                img = new Bitmap(Image.FromFile(filePath));
+                pictureBox1.Image = img;
+            }
+            else
+            {
+                //Console.WriteLine("キャンセルされました");
+            }
+
+            // オブジェクトを破棄する
+            ofDialog.Dispose();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            img = new Bitmap(Image.FromFile(filePath));
+            pictureBox1.Image = img;
+            checkBox1.Checked = false;
+        }
+
+        private void numClusters_ValueChanged(object sender, EventArgs e)
+        {
+            runKMeans();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                runKMeans();
+            }
+        }
+    }
+}
